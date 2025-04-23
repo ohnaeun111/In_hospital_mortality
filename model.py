8393f6a15537ba6169482d71d1891165ddd250b3
ohnaeun111 <jane3351@khu.ac.kr>
2025-04-23 09:19:07 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,46 +1,38 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "0dc97408-db8c-4edd-ab16-403e099862c6",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from xgboost import XGBClassifier\n",
-    "from lightgbm import LGBMClassifier\n",
-    "from sklearn.ensemble import GradientBoostingClassifier\n",
-    "\n",
-    "def get_models():\n",
-    "    \"\"\"\n",
-    "    Initialize three classifiers: XGBoost, LightGBM, and Gradient Boosting.\n",
-    "    \"\"\"\n",
-    "    model1 = XGBClassifier(n_estimators=300, learning_rate=0.05, max_depth=3, scale_pos_weight=5)\n",
-    "    model2 = LGBMClassifier(n_estimators=300, learning_rate=0.01, max_depth=7, min_split_gain=0.01, scale_pos_weight=5)\n",
-    "    model3 = GradientBoostingClassifier(n_estimators=600, learning_rate=0.01, max_depth=4, min_samples_split=2)\n",
-    "    return model1, model2, model3"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "onekernel",
-   "language": "python",
-   "name": "onefirst"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.19"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, BatchNormalization, Dropout
+from tensorflow.keras import losses, optimizers
+import scipy.io as sio
+import numpy as np
+from tensorflow.keras.models import load_model
+import joblib
+import os
+
+def eval_model(input_data, SavePath):
+    print(f"SavePath: {SavePath}") 
+    n_folds = 5
+    all_fold_models = []
+ 
+    for num_folds in range(1, n_folds + 1):
+        model_path = os.path.join(SavePath, f'model_fold{num_folds}.h5')
+        print(f"Trying to load model from: {model_path}")
+        if not os.path.exists(model_path):
+            print(f"Error: File does not exist - {model_path}")
+            continue
+        loaded_models= load_model(model_path)
+        all_fold_models.append(loaded_models)
+
+    if not all_fold_models:
+        raise FileNotFoundError("No models could be loaded. Please check the model paths.")
+
+    test_eval_probas = []
+
+    for model in all_fold_models:
+        probas = model.predict(input_data) 
+        test_eval_probas.append(probas)
+
+    final_test_proba = np.mean(test_eval_probas, axis=0)
+
+    # 최종 예측 확률 전체를 반환
+    return final_test_proba
